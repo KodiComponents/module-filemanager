708a987c49a0b668a78b20e84b96bc99e78029a8
Butschster <butschster@gmail.com>
2016-05-23 12:44:59 +0300

    fix permissions

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,8 +1,12 @@
 <?php
 
-Route::group(['prefix' => backend_url_segment(), 'as' => 'backend.', 'middleware' => ['web']], function () {
+Route::group(['prefix' => backend_url_segment(), 'as' => 'backend.', 'middleware' => ['web', 'backend.auth']], function () {
     Route::get('filemanager.popup', ['as' => 'filemanager.popup', 'uses' => 'FilemanagerController@popup']);
     Route::get('filemanager', ['as' => 'filemanager', 'uses' => 'FilemanagerController@show']);
 });
 
-RouteAPI::any('filemanager', ['as' => 'backend.filemanager.api', 'uses' => 'API\FilemanagerController@load', 'middleware' => ['web', 'api']]);
+RouteAPI::any('filemanager', [
+    'as' => 'backend.filemanager.api',
+    'uses' => 'API\FilemanagerController@load',
+    'middleware' => ['web', 'api', 'backend.auth']
+]);
